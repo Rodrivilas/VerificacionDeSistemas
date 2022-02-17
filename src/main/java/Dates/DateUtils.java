4c7fdf243aef2d340bd9163cd576631968b4e9c8
Rodrigo Viladegut <rodriviladegut@gmail.com>
2022-02-17 08:03:43 -0400

    Update Tests DateUtils

--- src/main/java/Dates/DateUtils.java
+++ src/main/java/Dates/DateUtils.java
@@ -4,88 +4,99 @@ public class DateUtils {
 
 
 
-    public String nextDate(int fecha,String mes,int anio){
-        if(fecha ==0 || fecha >31){
-            return "Dia incorrecto";
+    public String nextDate(int fecha,String mes,int anio) {
+        String mensaje = "";
+        int errores = 0;
+
+        if (fecha <= 0 || fecha > 31) {
+            mensaje = "Dia incorrecto";
+            errores++;
         }
-        if(anio ==0 || anio >9999){
-            return "Anio incorrecto";
-        }
-        if(mes.equals("Enero") && fecha>=1 && fecha <=31){
-            if(fecha == 31){
-                return 1 +" Febrero " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Febrero") && fecha>=1 && fecha <= bisiesto(anio)){
-            if(fecha == bisiesto(anio)){
-                return 1 +" Marzo " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Marzo") && fecha>=1 && fecha <= 31){
-            if(fecha == 31){
-                return 1 +" Abril " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Abril") && fecha>=1 && fecha <= 30){
-            if(fecha == 30){
-                return 1 +" Mayo " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Mayo") && fecha>=1 && fecha <= 31){
-            if(fecha == 31){
-                return 1 +" Junio " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Junio") && fecha>=1 && fecha <= 30){
-            if(fecha == 30){
-                return 1 +" Julio " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Julio") && fecha>=1 && fecha <= 31){
-            if(fecha == 31){
-                return 1 +" Agosto " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Agosto") && fecha>=1 && fecha <= 31){
-            if(fecha == 31){
-                return 1 +" Septiembre " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Septiembre") && fecha>=1 && fecha <= 30){
-            if(fecha == 30){
-                return 1 +" Octubre " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Octubre") && fecha>=1 && fecha <= 31){
-            if(fecha == 31){
-                return 1 +" Noviembre " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Noviembre") && fecha>=1 && fecha <= 30){
-            if(fecha == 30){
-                return 1 +" Diciembre " +anio;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
-        }else if(mes.equals("Diciembre") && fecha>=1 && fecha <= 31){
-            if(fecha == 31){
-                return 1 +" Enero " + anio+1;
-            }else{
-                return fecha+1 + " "+ mes + " "+anio;
-            }
+        if (anio <= 0 || anio > 9999) {
+            mensaje = "Anio incorrecto";
+            errores++;
         }
 
-        return "Mes incorrecto";
+        if(errores >1){
+            mensaje="Ingresar nuevos datos";
+        }else if(errores ==0) {
+            if (mes.equals("Enero") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Febrero " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Febrero") && fecha >= 1 && fecha <= bisiesto(anio)) {
+                if (fecha == bisiesto(anio)) {
+                    mensaje = 1 + " Marzo " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Marzo") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Abril " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Abril") && fecha >= 1 && fecha <= 30) {
+                if (fecha == 30) {
+                    mensaje = 1 + " Mayo " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Mayo") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Junio " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Junio") && fecha >= 1 && fecha <= 30) {
+                if (fecha == 30) {
+                    mensaje = 1 + " Julio " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Julio") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Agosto " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Agosto") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Septiembre " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Septiembre") && fecha >= 1 && fecha <= 30) {
+                if (fecha == 30) {
+                    mensaje = 1 + " Octubre " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Octubre") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Noviembre " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Noviembre") && fecha >= 1 && fecha <= 30) {
+                if (fecha == 30) {
+                    mensaje = 1 + " Diciembre " + anio;
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " + anio;
+                }
+            } else if (mes.equals("Diciembre") && fecha >= 1 && fecha <= 31) {
+                if (fecha == 31) {
+                    mensaje = 1 + " Enero " + (anio + 1);
+                } else {
+                    mensaje = fecha + 1 + " " + mes + " " +anio;
+                }
+            }else{
+                mensaje = "Mes incorrecto";
+            }
+        }
+        return mensaje;
     }
 
 
--- src/test/java/dateUtilsTest/DateUtilsTest.java
+++ src/test/java/dateUtilsTest/DateUtilsTest.java
@@ -17,12 +17,25 @@ public class DateUtilsTest {
             "28,Febrero,2001,1 Marzo 2001",
             "64,Enero,2004,Dia incorrecto",
             "8,Enero,32004,Anio incorrecto",
-            "8,asddasd,2004,Mes incorrecto"
+            "8,Abril,2004,9 Abril 2004",
+            "8,Junio,2004,9 Junio 2004",
+            "8,Julio,2004,9 Julio 2004",
+            "8,Agosto,2004,9 Agosto 2004",
+            "8,Septiembre,2004,9 Septiembre 2004",
+            "8,Octubre,2004,9 Octubre 2004",
+            "8,Noviembre,2004,9 Noviembre 2004",
+            "8,Diciembre,2004,9 Diciembre 2004",
+            "31,Diciembre,2004,1 Enero 2005",
+            "31,ssadsda,2004,Mes incorrecto",
+            "65,Febrero,300212,Ingresar nuevos datos"
+
+
+
 
 
 
     })
-    public void DateUtilsTest(int dia, String mes, int anio, String expectedResult){
+    public void DateUtilsTestm(int dia, String mes, int anio, String expectedResult){
         DateUtils dateUtils = new DateUtils();
         String actualResult=dateUtils.nextDate(dia,mes,anio);
         Assertions.assertEquals(expectedResult,actualResult,"ERROR! En DateUtils");
